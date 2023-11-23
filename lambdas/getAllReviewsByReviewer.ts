d90f33f5fe80ab875fe00afc32757245a01cbf10
Saoirse O'Donovan <saoirseodonovan13@gmail.com>
2023-11-23 18:23:59 +0000

    WIP for getting reviews by name without movieId
    I am attempting to get all the reviews written by a specific reviewer
    without the movieId present in the url, i can get all reviews but not
    the review for a single reviewer yet.
    
    Signed-off-by: Saoirse O'Donovan <saoirseodonovan13@gmail.com>

---
+++ lambdas/getAllReviewsByReviewer.ts
@@ -0,0 +1,68 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbDocClient = createDDbDocClient();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event, context) => {
+  try {
+    console.log("Event: ", event);
+    
+const reviewerName = event?.pathParameters?.reviewerName;
+
+if (!reviewerName) {
+  return {
+    statusCode: 404,
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({ Message: "Invalid reviewer name" }),
+  };
+}
+
+    const commandInput = {
+      TableName: process.env.TABLE_NAME,
+      KeyConditionExpression: "reviewerName = :r",
+      ExpressionAttributeValues: {
+        ":r": reviewerName,
+      },
+    };
+
+    const commandOutput = await ddbDocClient.send(
+        new QueryCommand(commandInput)
+        );
+
+    return {
+      statusCode: 200,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        data: commandOutput.Items,
+      }),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return {
+      statusCode: 500,
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
